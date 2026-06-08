f8f77205be6e3bc5c30910b3e700d5f63705d2fe
dimas adriansah <dimasadriansah28@gmail.com>
2026-06-09 01:16:47 +0700

    update terbaru pagi ini

--- web-dinamis/test_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-$columns = DB::select('describe orders');
-foreach($columns as $col) {
-    echo $col->Field . " | " . $col->Type . "\n";
-}
